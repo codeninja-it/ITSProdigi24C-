72c1bc07f7439b2fd57ef0686e8d2c4fafe4870f
Daniele Simoncini <simoncini@codeninja.it>
2024-04-11 15:31:51 +0200

    Aggiungere i file di progetto.

---
+++ PrimaApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft;
+using Microsoft.CSharp;
+using Microsoft.CSharp.RuntimeBinder;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Timers;
+using System.Web;
+using System.Xml;
+
+namespace PrimaApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // saluto
+            Console.WriteLine("Benvenuto!");
+
+            // chiedo il nome
+            Console.Write("In che file scrivo? ");
+            // e lo acquisisco
+            string? nomeFile = Console.ReadLine();
+
+            // chiedo il contenuto
+            Console.Write("Cosa ci metto dentro?");
+            // e lo acquisisco da console
+            string? contenuto = Console.ReadLine();
+
+            // se la cartella non esiste
+            if (!Directory.Exists("c:\\test"))
+                // la creo
+                Directory.CreateDirectory("c:\\test");
+            nomeFile = Path.Combine("c:\\test", nomeFile);
+
+            // poi vi scrivo dentro
+            File.WriteAllText(nomeFile, contenuto);
+        }
+    }
+}
---
+++ SecondaApp/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SecondaApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parmetri ricevuti:");
+            for(int i=0; i < args.Length; i++)
+            {
+                Console.WriteLine("- " + args[i]);
+            }
+
+            /*
+             1° argomento, cassetto 0 : nome
+             2° argomento, cassetto 1 : cognome
+             3° argomento, cassetto 2 : telefono
+             */
+
+            if (!Directory.Exists("contatti"))
+                Directory.CreateDirectory("contatti");
+
+            if(args.Length > 2)
+            {
+                string nome = args[0];
+                string cognome = args[1];
+                string telefono = args[2];
+
+                string nomeFile = Path.Combine("contatti", nome + "_" + cognome + ".vcf");
+                string buffer = "BEGIN:VCARD\n";
+                buffer += "VERSION:4.0\n";
+                buffer += "FN:" + nome + " " + cognome + "\n";
+                buffer += "TEL:" + telefono + "\n";
+                buffer += "END:VCARD";
+                File.WriteAllText(nomeFile, buffer);
+            }
+        }
+    }
+}
