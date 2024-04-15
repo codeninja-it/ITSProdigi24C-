f5511936b07d9cc21193b1f514e047dd9bb8091a
Daniele Simoncini <simoncini@codeninja.it>
2024-04-15 17:55:49 +0200

    risoluzione esercizio di creazione e gestione rubrica per classi

--- QuintaApp/strutture/Contatto.cs
+++ QuintaApp/strutture/Contatto.cs
@@ -8,5 +8,42 @@ namespace QuintaApp.strutture
 {
     internal class Contatto
     {
+        public string nome;
+        public string cognome;
+        public string telefono;
+
+        internal Contatto()
+        {
+            nome = Domanda("Che nome ha?");
+            cognome = Domanda("il suo cognome?");
+            telefono = Domanda("che telefono ci metto?");
+        }
+
+        private string Domanda(string testo)
+        {
+            Console.Write(testo);
+            return Console.ReadLine();
+        }
+
+        internal Contatto(string nome, string cognome, string telefono)
+        {
+            this.nome = nome;
+            this.telefono = telefono;
+            this.cognome = cognome;
+        }
+
+        // "Jhon    Doe     12342345"
+        internal Contatto(string flussoTestuale)
+        {
+            string[] pezzi = flussoTestuale.Split("\t");
+            this.nome = pezzi[0];
+            this.cognome = pezzi[1];
+            this.telefono = pezzi[2];
+        }
+
+        public override string ToString()
+        {
+            return $"{nome}\t{cognome}\t{telefono}";
+        }
     }
 }
--- QuintaApp/strutture/Rubrica.cs
+++ QuintaApp/strutture/Rubrica.cs
@@ -10,16 +10,17 @@ namespace QuintaApp.strutture
     {
         private List<Contatto> contatti = new List<Contatto>();
 
-        public Rubrica() { 
-            contatti.Clear();
-            Contatto nuovo = new Contatto();
-            contatti.Add( nuovo );
-            
-            for(int i=0; i < contatti.Count; i++)
+        public Rubrica(string path = "contatti.tab") {
+            if (File.Exists(path))
             {
-                Console.WriteLine(contatti[i]);
+                string buffer = File.ReadAllText(path);
+                string[] righe = buffer.Split("\n");
+            
+                for(int i=0; i < righe.Length; i++)
+                {
+                    contatti.Add(new Contatto( righe[i] ) );
+                }
             }
-
         }
 
         internal void gestisci(string comando)
@@ -27,21 +28,57 @@ namespace QuintaApp.strutture
             switch (comando)
             {
                 case "add":
-                    break;
+                    contatti.Add(new Contatto());
+                break;
 
                 case "del":
-                    break;
+                    Console.Write("Chi devo cancellare?");
+                    string nomeDaCancellare = Console.ReadLine();
+                    Contatto daCancellare = null;
+                    foreach(Contatto singolo in contatti)
+                    {
+                        if(singolo.nome == nomeDaCancellare)
+                        {
+                            daCancellare = singolo;
+                        }
+                    }
+                    if(daCancellare != null)
+                    {
+                        contatti.Remove(daCancellare);
+                    }
+                break;
+
+                case "save":
+                    string buffer = string.Join("\n", contatti);
+                    File.WriteAllText("contatti.tab", buffer);
+                break;
 
                 case "mod":
+                    Console.Write("Chi devo modificare?");
+                    string nomeDaModificare = Console.ReadLine();
+                    Contatto daModificare = null;
+                    foreach (Contatto singolo in contatti)
+                    {
+                        if (singolo.nome == nomeDaModificare)
+                        {
+                            daModificare = singolo;
+                        }
+                    }
+                    if (daModificare != null)
+                    {
+                        Console.WriteLine($"Attualmente il telefono è :{daModificare.telefono}");
+                        Console.Write("Nuovo numero:");
+                        daModificare.telefono = Console.ReadLine();
+                    }
                     break;
 
                 case "exit":
                     Console.WriteLine("Arrivederci!");
-                    break;
+                break;
 
                 default:
                     Console.WriteLine("Non ho capito...");
-                    break;
+                break;
             }
         }
     }
