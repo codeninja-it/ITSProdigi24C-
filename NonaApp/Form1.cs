dc63ab580a2eeeca5d9c0afd8b5569fdf3bcb14c
Daniele Simoncini <simoncini@codeninja.it>
2024-05-27 13:06:55 +0200

    agigunta alla nona app di un algoritmo di edging per nearest neighbour, oltre all'algoritmo di tersholding, utili alla preparazione dell'immagine per il calcolo di poligoni chiusi tramite Moore

--- NonaApp/Form1.cs
+++ NonaApp/Form1.cs
@@ -23,20 +23,18 @@ namespace NonaApp
             }
         }
 
-        private void btnCalcola_Click(object sender, EventArgs e)
+        private void CalcolaTreshold(Bitmap immagine, float da, float a)
         {
-            Bitmap immagine = new Bitmap(btnApri.Text);
-            float bottom = trkHueStart.Value;
-            float top = trkHueStop.Value;
             int positivi = 0;
-            txtRisultato.Text = $"threshold su hue da {bottom} a {top}\r\n";
+            txtRisultato.Text = $"threshold su hue da {da} a {a}\r\n";
             for (int x = 0; x < immagine.Width; x++)
             {
                 for (int y = 0; y < immagine.Height; y++)
                 {
+                    // calcolo del threshold
                     Color trovato = immagine.GetPixel(x, y);
                     float hue = trovato.GetHue();
-                    if (hue >= bottom && hue < top)
+                    if (hue >= da && hue < a)
                     {
                         immagine.SetPixel(x, y, Color.White);
                         positivi++;
@@ -50,7 +48,41 @@ namespace NonaApp
             double percentuale = ((double)positivi / (immagine.Width * immagine.Height)) * 100;
             txtRisultato.Text += $"Fatto: {percentuale:N2}%";
             pctAnteprima.Image = immagine;
-            immagine.Save(btnApri.Text.Replace(".png", $".{bottom:N0}.{top:N0}.png"), ImageFormat.Png);
+            immagine.Save(btnApri.Text.Replace(".png", $".{da:N0}.{a:N0}.png"), ImageFormat.Png);
+        }
+
+        private void CalcolaBordi(Bitmap immagine)
+        {
+            Bitmap bordi = new Bitmap(immagine.Width, immagine.Height);
+            for(int x = 0;x < immagine.Width - 1; x++)
+            {
+                for( int y = 0;y < immagine.Height - 1; y++)
+                {
+                    // recupero il pixel
+                    Color analizzato = immagine.GetPixel(x, y);
+                    // ed i suoi vicini (destra e sotto)
+                    Color vicinoDX = immagine.GetPixel(x + 1, y);
+                    Color vicinoBT = immagine.GetPixel(x ,y + 1);
+                    // se sono differenti
+                    if(analizzato.R != vicinoDX.R || analizzato.R != vicinoBT.R)
+                    {
+                        bordi.SetPixel(x, y, Color.Black);
+                    } else
+                    {
+                        bordi.SetPixel(x, y, Color.White);
+                    }
+                }
+            }
+            bordi.Save(btnApri.Text.Replace(".png", ".bordi.png"), ImageFormat.Png) ;
+        }
+
+        private void btnCalcola_Click(object sender, EventArgs e)
+        {
+            Bitmap immagine = new Bitmap(btnApri.Text);
+            float bottom = trkHueStart.Value;
+            float top = trkHueStop.Value;
+            CalcolaTreshold(immagine, bottom, top);
+            CalcolaBordi(immagine);
         }
 
         private void trkHueStart_Scroll(object sender, EventArgs e)
