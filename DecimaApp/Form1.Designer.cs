d5dba282d8d442b5c8f74f986b4527dd1a459216
Daniele Simoncini <simoncini@codeninja.it>
2024-06-10 17:50:26 +0200

    Ultimato esercizio su algoritmi ARIMA (auto regrssive integrated moving average) con possibilità di decidere quanti elementi precedenti prendere in considerazione per il calcolo dell'approssimazione

--- DecimaApp/Form1.Designer.cs
+++ DecimaApp/Form1.Designer.cs
@@ -28,21 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtPunti = new TextBox();
+            trkBuffer = new TrackBar();
+            ((System.ComponentModel.ISupportInitialize)trkBuffer).BeginInit();
             SuspendLayout();
+            // 
+            // txtPunti
+            // 
+            txtPunti.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtPunti.Location = new Point(12, 12);
+            txtPunti.Name = "txtPunti";
+            txtPunti.Size = new Size(1618, 39);
+            txtPunti.TabIndex = 0;
+            // 
+            // trkBuffer
+            // 
+            trkBuffer.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            trkBuffer.Location = new Point(12, 57);
+            trkBuffer.Maximum = 100;
+            trkBuffer.Minimum = 1;
+            trkBuffer.Name = "trkBuffer";
+            trkBuffer.Size = new Size(1618, 90);
+            trkBuffer.TabIndex = 1;
+            trkBuffer.Value = 10;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1113, 617);
+            ClientSize = new Size(1642, 1230);
+            Controls.Add(trkBuffer);
+            Controls.Add(txtPunti);
             Name = "Form1";
             Text = "Form1";
+            ResizeEnd += Form1_ResizeEnd;
             MouseDown += Form1_MouseDown;
             MouseMove += Form1_MouseMove;
             MouseUp += Form1_MouseUp;
+            ((System.ComponentModel.ISupportInitialize)trkBuffer).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtPunti;
+        private TrackBar trkBuffer;
     }
 }
--- DecimaApp/Form1.cs
+++ DecimaApp/Form1.cs
@@ -5,15 +5,23 @@ namespace DecimaApp
         private bool mouseGiu = false;
         private Bitmap sfondo;
 
+        private Point?[] buffer = new Point?[10];
+        private Point ultimo;
+        private Point ultimoARIMA;
+        private int indice = 0;
+
         public Form1()
         {
             InitializeComponent();
             sfondo = new Bitmap(this.Width, this.Height);
-        }        
+        }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
             mouseGiu = true;
+            ultimo = e.Location;
+            buffer = new Point?[trkBuffer.Value];
+            ultimoARIMA = PuntoMedio(ultimo);
         }
 
         private void Form1_MouseUp(object sender, MouseEventArgs e)
@@ -21,25 +29,48 @@ namespace DecimaApp
             mouseGiu = false;
         }
 
+        private Point PuntoMedio(Point nuovo)
+        {
+            // calcolo il nuovo Indice
+            indice = (indice + 1) % trkBuffer.Value;
+            buffer[indice] = nuovo;
+            // restituisco il punto medio
+            int Xmedia = (int)buffer.Where(p => p != null).Average(p => p.Value.X);
+            int Ymedia = (int)buffer.Where(p => p != null).Average(p => p.Value.Y);
+            return new Point(Xmedia, Ymedia);
+        }
+
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
             if (mouseGiu)
             {
-                
+
                 this.Text = $"Coordinate : {e.X} {e.Y}";
                 Graphics pennello = Graphics.FromImage(sfondo);
 
                 // sostituire setPixel con DrawLine
-                sfondo.SetPixel(e.X, e.Y, Color.Black);
-                pennello.DrawLine(Pens.Black, new Point(0, 0), new Point(100, 100));
-                
+                //sfondo.SetPixel(e.X, e.Y, Color.Black);
+                Point attuale = e.Location;
+                Point attualeARIMA = PuntoMedio(attuale);
+                pennello.DrawLine(new Pen(Color.Black, 5), ultimo, attuale);
+                pennello.DrawLine(new Pen(Color.Red, 5), ultimoARIMA, attualeARIMA);
+                ultimo = attuale;
+                ultimoARIMA = attualeARIMA;
+
+                txtPunti.Text = string.Join(",", buffer.Where(p => p != null).Select(p => $"[{p.Value.X} {p.Value.Y}]"));
+
                 // e disergnare anche la linea che ottengo tramite un
                 // algoritmo ARIMA a 10 elementi
 
                 // refresh di pagina
                 this.BackgroundImage = sfondo;
                 this.Invalidate();
-            }    
+            }
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            sfondo = new Bitmap(this.Width, this.Height);
         }
     }
 }
